34aedd33208aab6b22ca6e5e966a3809ea28cce1
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-06-04 17:47:21 +0200

    enable smart indentation

--- lua/config.lua
+++ lua/config.lua
@@ -2,22 +2,34 @@
 vim.g.markdown_fenced_languages = { 'html', 'python', 'bash=sh', 'R=r' }
 vim.opt.termguicolors = true
 
--- mor opinionated
+-- more opinionated
 vim.opt.number = true -- show linenumbers
 vim.opt.updatetime = 250 -- for autocommands and hovers
 vim.opt.mouse= 'a' -- enable mouse
 vim.opt.mousefocus = true
 vim.opt.clipboard:append 'unnamedplus' -- use system clipboard
+
 -- use spaces as tabs
 local tabsize = 2
 vim.opt.expandtab = true
 vim.opt.shiftwidth = tabsize
 vim.opt.tabstop = tabsize
+
+-- space as leader
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+-- smarter search
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
--- colortheme
+-- colortheme tweaks
 vim.g.nord_borders = true
+
+-- indent
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+-- consisten number column
+vim.opt.signcolumn = "yes:1"
 
