4fd851d52ae3ad1627352251f958d880cc8f97ad
Jannik Buhr <jannik.m.buhr@gmail.com>
2022-09-14 10:53:52 +0200

    use bracketed paste with tmux instead of dedicated python pasting

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -78,7 +78,7 @@ require('packer').startup {
     use { 'jpalardy/vim-slime',
       config = function()
         vim.g.slime_target = "tmux"
-        vim.g.slime_python_ipython = 1
+        vim.g.slime_bracketed_paste = 1
         vim.g.slime_default_config = { socket_name = "default", target_pane = ":.2" }
         vim.b.slime_cell_delimiter = "#%%"
       end
