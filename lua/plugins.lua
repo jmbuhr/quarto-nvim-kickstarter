1768404880b84882f540084589a77841a49cbf2d
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-01-03 15:49:56 +0100

    don't use local dev version of quarto and otter

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,18 +1,15 @@
 return {
 
   { 'quarto-dev/quarto-nvim',
-    dev = true,
     dependencies = {
-      { 'jmbuhr/otter.nvim',
-        dev = true,
-      },
+      { 'jmbuhr/otter.nvim' },
       'neovim/nvim-lspconfig'
     },
     config = function()
       require 'quarto'.setup {
         lspFeatures = {
           enabled = true,
-          languages = { 'r', 'python', 'julia', 'haskell', 'lua' },
+          languages = { 'r', 'python', 'julia' },
           diagnostics = {
             enabled = true,
             triggers = { "BufWrite" }
