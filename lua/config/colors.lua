f3e04562860b61e9bbeb9d87059d60436e9a6ccc
Joanes Grandjean <22633767+grandjeanlab@users.noreply.github.com>
2023-02-08 11:34:29 +0100

    frappe all the things

--- lua/config/colors.lua
+++ lua/config/colors.lua
@@ -1,5 +1,5 @@
 
 vim.cmd.colorscheme 'catppuccin'
-local colors = require('catppuccin.palettes.mocha')
+local colors = require('catppuccin.palettes.frappe')
 vim.cmd.highlight { 'Tabline', 'guifg=' .. colors.green, 'guibg=' .. colors.mantle }
 
