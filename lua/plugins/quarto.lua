0756a76273420edf78c9919ef9a58c180fd07f54
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-04-03 17:02:18 +0200

    feat: use ipython cpaste when in python code chunk

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -1,10 +1,13 @@
 return {
   { 'quarto-dev/quarto-nvim',
+    dev = false,
     dependencies = {
       { 'hrsh7th/nvim-cmp' },
       { 'neovim/nvim-lspconfig' },
       { 'nvim-treesitter/nvim-treesitter' },
       { 'jmbuhr/otter.nvim',
+        tag = nil,
+        branch = 'nightly-treesitter',
         config = function()
           require 'otter.config'.setup {
             lsp = {
@@ -75,6 +78,54 @@ return {
   -- like ipython, R, bash
   { 'jpalardy/vim-slime',
     init = function()
+
+      Quarto_is_in_python_chunk = function()
+        vim.b.quarto_is_python_chunk = false
+        local ts = vim.treesitter
+        local language_tree = ts.get_parser(0, 'markdown')
+        local syntax_tree = language_tree:parse()
+        local root = syntax_tree[1]:root()
+
+        -- create capture
+        local query = vim.treesitter.query.parse('markdown', require 'otter.tools.queries'['markdown'])
+
+        local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+        row = row - 1
+        col = col
+
+        -- get text ranges
+        for pattern, match, metadata in query:iter_matches(root, 0) do
+          -- each match has two nodes, the language and the code
+          -- the language node is the first one
+          local found = false -- reset found for the next match
+          for id, node in pairs(match) do
+            local name = query.captures[id]
+            local ok, text = pcall(vim.treesitter.get_node_text, node, 0)
+            if not ok then return false end
+            if name == 'lang' and text == 'python' then
+              -- we found a match where the language node matches
+              -- the otter language
+              found = true
+            end
+            -- the corresponding code is in the current range
+            if found and name == 'code' and ts.is_in_node_range(node, row, col) then
+              vim.b.quarto_is_python_chunk = true
+            end
+          end
+        end
+      end
+
+      vim.cmd [[
+      function SlimeOverride_EscapeText_quarto(text)
+        call v:lua.Quarto_is_in_python_chunk() 
+        if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
+          return ["%cpaste -q", "\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+        else
+          return a:text
+        end
+      endfunction
+      ]]
+
       vim.b.slime_cell_delimiter = "#%%"
       -- -- slime, tmux
       -- vim.g.slime_target = 'tmux'
@@ -83,8 +134,7 @@ return {
 
       -- slime, neovvim terminal
       vim.g.slime_target = "neovim"
-      -- vim.g.slime_python_ipython = 1
-      --
+      vim.g.slime_python_ipython = 1
     end
   },
   -- paste an image to markdown from the clipboard
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,9 +1,11 @@
 return {
   { 'nvim-treesitter/nvim-treesitter',
+    tag = nil,
+    branch = 'master',
     run = ':TSUpdate',
     config = function()
       require 'nvim-treesitter.configs'.setup {
-        ensure_installed = { 'r', 'python', 'markdown', 'markdown_inline', 'julia', 'yaml', 'lua', 'vim', 'query', 'help',
+        ensure_installed = { 'r', 'python', 'markdown', 'markdown_inline', 'julia', 'yaml', 'lua', 'vim', 'query', 'help', 'vimdoc',
           'latex' },
         highlight = {
           enable = true,
