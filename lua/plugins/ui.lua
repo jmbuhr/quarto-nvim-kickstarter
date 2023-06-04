324bd24e6bec23a70463e0aebf7368c898822298
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-06-04 09:56:03 +0200

    indent-blankline

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -154,11 +154,11 @@ return {
     require("trouble").setup {}
   end
   },
-  -- { 'lukas-reineke/indent-blankline.nvim', config = function()
-  --   require("indent_blankline").setup {
-  --     show_current_context = true,
-  --     show_current_context_start = false,
-  --   }
-  -- end
-  -- },
+  { 'lukas-reineke/indent-blankline.nvim', config = function()
+    require("indent_blankline").setup {
+      show_current_context = true,
+      show_current_context_start = false,
+    }
+  end
+  },
 }
