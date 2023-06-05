896c432ba0ad5676bf36c918f050f9193a87586b
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-06-05 11:45:20 +0200

    fix: remove (deprecated?) vim-slime option

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -528,6 +528,8 @@ return {
   {
     'jpalardy/vim-slime',
     init = function()
+
+      vim.b['quarto_is_' .. 'python' .. '_chunk'] = false
       Quarto_is_in_python_chunk = function()
         require 'otter.tools.functions'.is_otter_language_context('python')
       end
@@ -536,7 +538,7 @@ return {
       function SlimeOverride_EscapeText_quarto(text)
       call v:lua.Quarto_is_in_python_chunk()
       if exists('g:slime_python_ipython') && len(split(a:text,"\n")) > 1 && b:quarto_is_python_chunk
-      return ["%cpaste -q", "\n", g:slime_dispatch_ipython_pause, a:text, "--", "\n"]
+      return ["%cpaste -q", "\n", a:text, "--", "\n"]
       else
       return a:text
       end
@@ -552,7 +554,7 @@ return {
         vim.b.slime_config = { jobid = vim.g.slime_last_channel }
       end
 
-      vim.b.slime_cell_delimiter = "#%%"
+      vim.b.slime_cell_delimiter = "# %%"
 
       -- slime, neovvim terminal
       vim.g.slime_target = "neovim"
