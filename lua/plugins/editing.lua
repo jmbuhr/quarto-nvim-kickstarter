49d91fdfa4e17493d68f7a115c9684b477302fba
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-06-19 09:22:44 +0200

    use bigfile.nvim

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -1,4 +1,24 @@
 return {
+  {
+    'LunarVim/bigfile.nvim',
+    init = function()
+      -- default config
+      require("bigfile").setup {
+        filesize = 2, -- size of the file in MiB, the plugin round file sizes to the closest MiB
+        pattern = { "*" }, -- autocmd pattern
+        features = { -- features to disable
+          "indent_blankline",
+          "illuminate",
+          "lsp",
+          "treesitter",
+          "syntax",
+          "matchparen",
+          "vimopts",
+          "filetype",
+        },
+      }
+    end
+  },
   { 'tpope/vim-repeat' },
   {
     "kylechui/nvim-surround",
