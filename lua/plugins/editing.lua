898d3c172b0d4ad780e01a5dc406f67c42d898b2
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-06-24 11:28:43 +0200

    feat: leap -> flash

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -54,9 +54,24 @@ return {
     config = true, -- default settings
   },
   {
-    "ggandor/leap.nvim",
-    config = function()
-      require('leap').add_default_mappings()
-    end
+    "folke/flash.nvim",
+    event = "VeryLazy",
+    opts = {},
+    keys = {
+      {
+        "s",
+        mode = { "n", "x", "o" },
+        function()
+          require("flash").jump()
+        end,
+      },
+      {
+        "S",
+        mode = { "o", "x" },
+        function()
+          require("flash").treesitter()
+        end,
+      },
+    },
   }
 }
