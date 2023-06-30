7eb3d01d2113359820321be064ff0b0a1f378c43
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-06-30 11:40:17 +0200

    feat: highlight code chunks with headlines.nvim

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -164,4 +164,23 @@ return {
     end
   },
 
+  {
+    'lukas-reineke/headlines.nvim',
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = function()
+      require("headlines").setup {
+        quarto = {
+          query = vim.treesitter.query.parse(
+            "markdown",
+            [[
+                (fenced_code_block) @codeblock
+            ]]),
+          codeblock_highlight = "CodeBlock",
+          treesitter_language = "markdown",
+        },
+      }
+    end
+  }
+
+
 }
