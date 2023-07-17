ba10d9e9463a6c2505ab1d9c9c29e757affc0d70
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-07-17 18:16:39 +0200

    fix: fix on_attach

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -6,6 +6,9 @@ return {
     dependencies = {
       {
         'jmbuhr/otter.nvim',
+        dependencies = {
+          { 'neovim/nvim-lspconfig' },
+        },
         dev = false,
         opts = {
             lsp = {
@@ -45,24 +48,8 @@ return {
 
     },
     opts = {
-      closePreviewOnExit = true,
       lspFeatures = {
-        enabled = true,
-        chunks = 'curly',
         languages = { 'r', 'python', 'julia', 'bash', 'lua', 'html' },
-        diagnostics = {
-          enabled = true,
-          triggers = { "BufWritePost" }
-        },
-        completion = {
-          enabled = true,
-        },
-      },
-      keymap = {
-        hover = 'K',
-        definition = 'gd',
-        rename = '<leader>lR',
-        references = 'gr',
       },
     }
   },
@@ -183,21 +170,27 @@ return {
         client.server_capabilities.document_formatting = true
       end
 
+      local on_attach_qmd = function(client, bufnr)
+        local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+        local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-      -- local on_attach2 = function(client, bufnr)
-      --   local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-      --   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-      --
-      --   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
-      --   local opts = { noremap = true, silent = true }
-      --
-      --   buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-      --   buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
-      --   buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-      --   buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-      --   buf_set_keymap('n', '<leader>ll', '<cmd>lua vim.lsp.codelens.run()<cr>', opts)
-      --   client.server_capabilities.document_formatting = true
-      -- end
+        buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+        local opts = { noremap = true, silent = true }
+
+        -- buf_set_keymap('n', 'gS', '<cmd>Telescope lsp_document_symbols<CR>', opts)
+        -- buf_set_keymap('n', 'gD', '<cmd>Telescope lsp_type_definitions<CR>', opts)
+        -- buf_set_keymap('n', 'gd', '<cmd>Telescope lsp_definitions<CR>', opts)
+        -- buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+        buf_set_keymap('n', 'gh', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+        buf_set_keymap('n', 'gi', '<cmd>Telescope lsp_implementations<CR>', opts)
+        -- buf_set_keymap('n', 'gr', '<cmd>Telescope lsp_references<CR>', opts)
+        buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+        buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+        buf_set_keymap('n', '<leader>ll', '<cmd>lua vim.lsp.codelens.run()<cr>', opts)
+        buf_set_keymap('n', '<leader>lR', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        client.server_capabilities.document_formatting = true
+      end
+
 
       local lsp_flags = {
         allow_incremental_sync = true,
@@ -224,7 +217,7 @@ return {
       -- [core]
       -- markdown.file_extensions = ["md", "markdown", "qmd"]
       lspconfig.marksman.setup {
-        on_attach = on_attach,
+        on_attach = on_attach_qmd,
         capabilities = capabilities,
         filetypes = { 'markdown', 'quarto' },
         root_dir = util.root_pattern(".git", ".marksman.toml", "_quarto.yml"),
@@ -233,7 +226,7 @@ return {
       -- -- another optional language server for grammar and spelling
       -- -- <https://github.com/valentjn/ltex-ls>
       -- lspconfig.ltex.setup {
-      --   on_attach = on_attach2,
+      --   on_attach = on_attach_qmd,
       --   capabilities = capabilities,
       --   filetypes = { "markdown", "tex", "quarto" },
       -- }
