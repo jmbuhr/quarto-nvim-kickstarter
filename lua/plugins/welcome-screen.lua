ce92480f82228ec0896a1af94794c0116cb38841
DEAA <59418167+l4loLX3@users.noreply.github.com>
2023-08-29 01:52:29 -0600

    fix: Update welcome-screen.lua (#55)
    remove deprecated nerdfont icons

--- lua/plugins/welcome-screen.lua
+++ lua/plugins/welcome-screen.lua
@@ -20,10 +20,10 @@ return {
       -- Set menu
       dashboard.section.buttons.val = {
         dashboard.button("e", "  > New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("f", "  > Find file", ":Telescope find_files<CR>"),
+        dashboard.button("f", "󰈞  > Find file", ":Telescope find_files<CR>"),
         dashboard.button("r", "  > Recent", ":Telescope oldfiles<CR>"),
         dashboard.button("s", "  > Settings", ":e $MYVIMRC | :cd %:p:h<cr>"),
-        dashboard.button("q", "  > Quit NVIM", ":qa<CR>"),
+        dashboard.button("q", "󰅚  > Quit NVIM", ":qa<CR>"),
       }
 
       local fortune = require("alpha.fortune")
