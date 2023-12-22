205bcc98b4286c32334530c06d33a9a8419f314f
Jannik Buhr <jannik.m.buhr@gmail.com>
2023-12-22 11:28:07 +0100

    feat: add ls for dot graphviz

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -27,7 +27,7 @@ return {
     },
     opts = {
       lspFeatures = {
-        languages = { "r", "python", "julia", "bash", "lua", "html" },
+        languages = { "r", "python", "julia", "bash", "lua", "html", "dot" },
       },
     },
   },
@@ -54,6 +54,7 @@ return {
           "latex",
           "html",
           "css",
+          "dot"
         },
         highlight = {
           enable = true,
@@ -274,6 +275,12 @@ return {
             },
           },
         },
+      })
+
+      lspconfig.dotls.setup({
+        on_attach = on_attach,
+        capabilities = capabilities,
+        flags = lsp_flags,
       })
 
       local function strsplit(s, delimiter)
