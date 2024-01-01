dc7468b04ad9dc1d5f0b1b08a509a6d9298fc777
Joanes Grandjean <22633767+grandjeanlab@users.noreply.github.com>
2024-01-01 20:31:19 +0100

    treesitter

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,7 +1,7 @@
 return {
   { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate', config = function()
     require 'nvim-treesitter.configs'.setup {
-      ensure_installed = { 'r', 'python', 'markdown', 'markdown_inline', 'yaml', 'vim', 'query', 'help' },
+      ensure_installed = { 'r', 'python', 'markdown', 'markdown_inline', 'yaml', 'vim' },
       highlight = {
         enable = true,
         additional_vim_regex_highlighting = false,
