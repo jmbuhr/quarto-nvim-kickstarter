d62b8a97574ceb284b1424d02564326945ea4275
Aman9das <39594914+Aman9das@users.noreply.github.com>
2024-02-23 16:50:21 +0530

    fix: update img-clip plugin config for new version (#85)

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -710,18 +710,20 @@ return {
     "HakonHarnes/img-clip.nvim",
     event = "BufEnter",
     opts = {
-      markdown = {
-        url_encode_path = true,
-        template = "![$CURSOR]($FILE_PATH)",
-        drag_and_drop = {
-          download_images = false,
+      filetypes = {
+        markdown = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
         },
-      },
-      quarto = {
-        url_encode_path = true,
-        template = "![$CURSOR]($FILE_PATH)",
-        drag_and_drop = {
-          download_images = false,
+        quarto = {
+          url_encode_path = true,
+          template = "![$CURSOR]($FILE_PATH)",
+          drag_and_drop = {
+            download_images = false,
+          },
         },
       },
     },
