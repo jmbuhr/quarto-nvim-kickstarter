9e83e7678eb9e5c5f35a44f491ff026467ed625c
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-03-12 12:47:55 +0100

    move dropbar keymap to dropbar config

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -188,12 +188,14 @@ return {
 
   { -- or show symbols in the current file as breadcrumbs
     'Bekaboo/dropbar.nvim',
+    enabled = true,
     dependencies = {
       'nvim-telescope/telescope-fzf-native.nvim',
     },
     config = function()
       -- turn off global option for windowline
       vim.opt.winbar = nil
+      vim.keymap.set('n', '<leader>ls', require('dropbar.api').pick, { desc = '[s]ymbols' })
     end,
   },
 
