b2c085452b6dcc98e3b1554d3192e330ecc5cbc2
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-03-13 12:54:39 +0100

    fix: set math.randomseed to ensure a different animal in the status line every start

--- lua/config/global.lua
+++ lua/config/global.lua
@@ -64,7 +64,9 @@ let g:currentmode={
        \}
 ]]
 
-vim.opt.statusline = '%{%g:currentmode[mode()]%} %{%reg_recording()%} %* %t | %y | %* %= c:%c l:%l/%L %p%% ' .. animals[math.random(#animals)] .. ' %*'
+math.randomseed(os.time())
+local i = math.random(#animals)
+vim.opt.statusline = '%{%g:currentmode[mode()]%} %{%reg_recording()%} %* %t | %y | %* %= c:%c l:%l/%L %p%% ' .. animals[i] .. ' %*'
 
 -- hide cmdline when not used
 vim.opt.cmdheight = 1
