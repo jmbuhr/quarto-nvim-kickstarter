cb3e80b4a78fb3e3d8b423807b61dcfe1b07ca6c
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-04-04 12:07:45 +0200

    refactor(modules): refactore plugins for quarto
    into sensible modules

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,85 @@
+return {
+  {
+    'nvim-treesitter/nvim-treesitter',
+    dependencies = {
+      { 'nvim-treesitter/nvim-treesitter-textobjects' },
+    },
+    run = ':TSUpdate',
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('nvim-treesitter.configs').setup {
+        auto_install = true,
+        ensure_installed = {
+          'r',
+          'python',
+          'markdown',
+          'markdown_inline',
+          'julia',
+          'bash',
+          'yaml',
+          'lua',
+          'vim',
+          'query',
+          'vimdoc',
+          'latex', -- requires tree-sitter-cli (installed automatically via Mason)
+          'html',
+          'css',
+          'dot',
+          'javascript',
+          'mermaid',
+          'norg',
+          'typescript',
+        },
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        indent = {
+          enable = true,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = 'gnn',
+            node_incremental = 'grn',
+            scope_incremental = 'grc',
+            node_decremental = 'grm',
+          },
+        },
+        textobjects = {
+          select = {
+            enable = true,
+            lookahead = true,
+            keymaps = {
+              -- You can use the capture groups defined in textobjects.scm
+              ['af'] = '@function.outer',
+              ['if'] = '@function.inner',
+              ['ac'] = '@class.outer',
+              ['ic'] = '@class.inner',
+            },
+          },
+          move = {
+            enable = true,
+            set_jumps = true, -- whether to set jumps in the jumplist
+            goto_next_start = {
+              [']m'] = '@function.outer',
+              [']]'] = '@class.inner',
+            },
+            goto_next_end = {
+              [']M'] = '@function.outer',
+              [']['] = '@class.outer',
+            },
+            goto_previous_start = {
+              ['[m'] = '@function.outer',
+              ['[['] = '@class.inner',
+            },
+            goto_previous_end = {
+              ['[M'] = '@function.outer',
+              ['[]'] = '@class.outer',
+            },
+          },
+        },
+      }
+    end,
+  },
+}
