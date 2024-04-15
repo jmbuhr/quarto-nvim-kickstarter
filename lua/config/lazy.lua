ffdfbb524db8ebb74620fc45f5643377a9bdbc68
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-04-15 15:21:46 +0200

    cleanup

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -36,8 +36,7 @@ require('lazy').setup('plugins', {
     },
   },
   change_detection = {
-    -- automatically check for config file changes and reload the ui
     enabled = true,
-    notify = false, -- get a notification when changes are found
+    notify = false,
   },
 })
