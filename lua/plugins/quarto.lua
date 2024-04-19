d31943cbf8bddb9def4f1612761d2dac69c0ffeb
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-04-19 11:29:19 +0200

    feat: add jupytext

--- lua/plugins/quarto.lua
+++ lua/plugins/quarto.lua
@@ -22,6 +22,27 @@ return {
     },
   },
 
+  { -- directly open ipynb files as quarto docuements
+    -- and convert back behind the scenes
+    -- needs:
+    -- pip install jupytext
+    'GCBallesteros/jupytext.nvim',
+    opts = {
+      custom_language_formatting = {
+        python = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto', -- you can set whatever filetype you want here
+        },
+        r = {
+          extension = 'qmd',
+          style = 'quarto',
+          force_ft = 'quarto', -- you can set whatever filetype you want here
+        },
+      },
+    },
+  },
+
   { -- send code from python/r/qmd documets to a terminal or REPL
     -- like ipython, R, bash
     'jpalardy/vim-slime',
