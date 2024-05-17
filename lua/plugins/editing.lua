ed36dcd2c8048fef7a956def38f724fb27e889e7
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-05-17 10:02:58 +0200

    use builtin commenting for nvim >= 0.10

--- lua/plugins/editing.lua
+++ lua/plugins/editing.lua
@@ -11,11 +11,13 @@ return {
     opts = {},
   },
 
-  -- commenting with e.g. `gcc` or `gcip`
-  -- respects TS, so it works in quarto documents
-  {
+  { -- commenting with e.g. `gcc` or `gcip`
+    -- respects TS, so it works in quarto documents 'numToStr/Comment.nvim',
     'numToStr/Comment.nvim',
     version = nil,
+    cond = function()
+      return vim.fn.has 'nvim-0.10' == 0
+    end,
     branch = 'master',
     config = true,
   },
