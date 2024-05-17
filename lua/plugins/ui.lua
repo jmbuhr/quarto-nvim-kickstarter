4e3e3abf7d267cfa109ad3d9e1a9cb9d7b52184a
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-05-17 15:42:25 +0200

    feat: oil.nvim to edit the filesystem as a buffer

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -116,6 +116,26 @@ return {
     opts = { signs = false },
   },
 
+  { -- edit the file system as a buffer
+    'stevearc/oil.nvim',
+    opts = {
+      keymaps = {
+        ['<C-s>'] = false,
+        ['<C-h>'] = false,
+        ['<C-l>'] = false,
+      },
+      view_options = {
+        show_hidden = true,
+      },
+    },
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    keys = {
+      { '-', ':Oil<cr>', desc = 'oil' },
+      { '<leader>ef', ':Oil<cr>', desc = 'edit [f]iles' },
+    },
+    cmd = 'Oil',
+  },
+
   { -- statusline
     -- PERF: I found this to slow down the editor
     'nvim-lualine/lualine.nvim',
