bb3fa644d6516b4fc060042c74878f1af81d0774
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-05-18 12:51:43 +0200

    fix: no signcolumn for terminal windows

--- lua/config/autocommands.lua
+++ lua/config/autocommands.lua
@@ -16,6 +16,7 @@ vim.api.nvim_create_autocmd({ 'TermOpen' }, {
   pattern = { '*' },
   callback = function(_)
     vim.cmd.setlocal 'nonumber'
+    vim.wo.signcolumn = 'no'
     set_terminal_keymaps()
   end,
 })
