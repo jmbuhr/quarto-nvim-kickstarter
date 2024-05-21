0470708f6db17398e16e130ff84c497cb0bfc5fc
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-05-22 00:19:50 +0200

    remove signcolumn background color

--- lua/plugins/colorthemes.lua
+++ lua/plugins/colorthemes.lua
@@ -36,6 +36,17 @@ return {
     lazy = false,
     priority = 1000,
     config = function()
+      require('kanagawa').setup {
+        colors = {
+          theme = {
+            all = {
+              ui = {
+                bg_gutter = 'none',
+              },
+            },
+          },
+        },
+      }
       vim.cmd.colorscheme 'kanagawa'
       vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
     end,
