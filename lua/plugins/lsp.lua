77c96167260c6664e3a6a424af9f9b41ad86fb2c
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-06-28 12:38:54 +0200

    need this url line

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -144,7 +144,7 @@ return {
           yaml = {
             schemaStore = {
               enable = true,
-              -- url = '',
+              url = '',
             },
           },
         },
