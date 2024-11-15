f2efd21cc9dccf245f95a0b9b7af256fe5431e39
Jannik Buhr <jannik.m.buhr@gmail.com>
2024-11-15 10:35:40 +0100

    snacks

--- lua/plugins/common.lua
+++ lua/plugins/common.lua
@@ -1,4 +1,18 @@
 return {
   -- common dependencies
   { 'nvim-lua/plenary.nvim' },
+  {
+    "folke/snacks.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+      styles = {},
+      -- disables hungry features for files larget than 1.5MB
+      bigfile = { enabled = true },
+      notifier = { enabled = true },
+      quickfile = { enabled = true },
+      statuscolumn = { enabled = false },
+      words = { enabled = true },
+    },
+  },
 }
